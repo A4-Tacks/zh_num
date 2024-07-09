aa5770a1c7347452fdfe66d7cc7154963fb3075e
A4-Tacks <wdsjxhno1001@163.com>
2024-07-09 12:00:21 +0800

    增加选项-s, 可以方便的跳过前面的内容

--- src/main.rs
+++ src/main.rs
@@ -52,15 +52,36 @@ const NAME: &str = env!("CARGO_BIN_NAME");
 
 fn main() -> io::Result<()> {
     let mut rem = false;
+    let mut skip_ch = 0;
     let args = args()
         .skip(1)
         .filter(|arg| (arg == "-r").then(|| rem = true).is_none())
+        .filter(|arg| arg.starts_with("-s").then(|| {
+            skip_ch = arg[2..].parse().expect("Invalid -s")
+        }).is_none())
         .collect::<Vec<_>>();
+    macro_rules! skip_ch_line {
+        ($line:expr) => {{
+            fn convf<'a, T, F>(f: F) -> F
+            where T: ?Sized + 'a,
+                  F: FnOnce(&'a T) -> (&'a T, &'a T),
+            {
+                f
+            }
+            convf(|line: &str| {
+                line.char_indices()
+                    .nth(skip_ch)
+                    .map(|(i, _)| line.split_at(i))
+                    .unwrap_or((line, ""))
+            })($line)
+        }};
+    }
     let args = args.iter().map(|x| &**x).collect::<Vec<_>>();
     match args[..] {
         [] => {
             stdin().lines().try_for_each(|line| {
                 (|line| {
+                    let (prefix, line) = skip_ch_line!(line);
                     let (n, s) = number(line)
                         .map(|(n, s)| (Some(n), s))
                         .or_else(|e| {
@@ -68,9 +89,12 @@ fn main() -> io::Result<()> {
                                 e.location.column,
                                 e.expected
                             )?;
-                            io::Result::Ok((rem.then_some(0), line))
+                            io::Result::Ok((None, line))
                         })?;
                     let mut stdout = stdout().lock();
+                    if rem {
+                        write!(stdout, "{prefix}")?;
+                    }
                     if let Some(n) = n {
                         write!(stdout, "{n}")?;
                     }
@@ -83,18 +107,19 @@ fn main() -> io::Result<()> {
             })
         },
         ["-h" | "--help", ..] | [.., "-h" | "--help"] => {
-            eprintln!("USAGE: {NAME} [-r] [-d] [-h | --help]");
+            eprintln!("USAGE: {NAME} [-r | -s]... [-d] [-h | --help]");
             eprintln!("将ASCII数字和中文数字相互转换");
             eprintln!("OPTIONS:");
             eprintln!("    -d           反向转换, 也就是将ASCII数字转换成中文数字");
             eprintln!("    -r           转换时保留结果之后的文本");
+            eprintln!("    -s<num>      识别时跳过一部分字符, 如果给定了-r则会留在结果中");
             eprintln!("    -h, --help   显示帮助");
             Ok(())
         },
         ["-d"] => {
             stdin().lines().try_for_each(|line| {
                 let line = line?;
-                let line = line.trim();
+                let (prefix, line) = skip_ch_line!(line.trim());
                 let rem_idx = line
                     .find(|ch| !char::is_ascii_digit(&ch))
                     .unwrap_or(line.len());
@@ -106,7 +131,10 @@ fn main() -> io::Result<()> {
                         io::Result::Ok(0)
                     })?;
 
-                let mut stdout = IOFmtWrapper::new(stdout());
+                let mut stdout = IOFmtWrapper::new(stdout().lock());
+                if rem {
+                    stdout.io_write_str(prefix);
+                }
                 fmt_zh_num(num, &mut stdout).unwrap();
                 if rem {
                     stdout.io_write_str(rem_str);
